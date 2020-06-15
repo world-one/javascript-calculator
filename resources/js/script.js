6f136c2c623f57d0f9ca6559f5685d030b06d79f
world-one <worldone85@gmail.com>
2020-06-15 12:22:46 +0900

    [20200615]

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,14 @@
 class Calculator{
-  constructor( targetElId, inputElId ){
+  
+  TYPE_OP = 'operator';
+  TYPE_DOT = 'dot';
+  
+  constructor( targetElId, processElId, resultElId ){    
     this.keypadEl = document.getElementById(targetElId); 
-    this.inputEl = document.getElementById(inputElId);
+    this.processEl = document.getElementById(processElId);
+    this.resultEl = document.getElementById(resultElId);
     this.inputValueArr = [];
-    this.prevInputType;
+    this.inputTypeArr = [];
     this.init();
   }
 
@@ -17,41 +22,74 @@ class Calculator{
     if( btn.dataset.keypadType != undefined ){
       const btnType = btn.dataset.keypadType;
       const btnValue = btn.dataset.value;
-      this[btnType](btnValue);
-      this.prevInputType = btnType;
+      this.clicked[btnType](btnValue, btnType);
     };
   }
-
-  number(btnValue){
+  
+  clicked = {
+    number: (btnValue, btnType) => {
+      this.pushValueArr(btnValue, btnType);
+    },
+    operator: (btnValue, btnType) => {
+      if( this.getPrevType() !== this.TYPE_OP ){
+        this.pushValueArr(btnValue, btnType);
+      }
+    },
+    bracket: (btnValue, btnType) => {
+      if( this.prevInputType && 
+        ( this.prevInputType !== this.TYPE_OP && this.prevInputType !== 'bracket' ) ){
+          btnValue = ')';
+      }
+      this.pushValueArr(btnValue, btnType);
+    },
+    dot: (btnValue, btnType) => {
+      if(this.checkDot() !== this.TYPE_DOT ){
+        this.pushValueArr(btnValue, btnType);
+      };
+    },
+    clear: (btnValue) => {
+      if(btnValue > 0) {
+        this.inputValueArr.pop();
+        this.inputTypeArr.pop();
+      }else{
+        this.inputValueArr = [];
+        this.inputTypeArr = [];
+      } 
+      this.insertValueProcessEl();
+    },
+    calculation: () => {
+      if( this.getPrevType() === this.OP ){
+        this.inputValueArr.pop();
+        this.insertValueProcessEl();
+      }
+      this.resultEl.value = eval( this.processEl.value );
+    },
+  }
+  pushValueArr(btnValue, btnType){
     this.inputValueArr.push(btnValue);
-    this.insertInputElValue()
+    this.inputTypeArr.push(btnType);
+    this.insertValueProcessEl();
   }
-
-  operator(btnValue){
-    if( this.prevInputType && this.prevInputType !== 'operator' ){
-      this.inputValueArr.push(btnValue);
-      this.insertInputElValue();
+  insertValueProcessEl(){
+    this.processEl.value  = this.inputValueArr.join('');
+  }
+  getPrevType(){
+    const arrLen = this.inputTypeArr.length;
+    if(arrLen === 0) return false;
+    return this.inputTypeArr[arrLen-1];
+  }
+  checkDot(){
+    let type = '';
+    for( let i = this.inputTypeArr.length - 1; i > -1; i-- ){
+      type = this.inputTypeArr[i];
+      if( type === this.TYPE_OP || type === this.TYPE_DOT ) break;
     }
-  }
-
-  clear(btnValue){
-    if(btnValue > 0) this.inputValueArr.pop();
-    else             this.inputValueArr = [];
-    
-    this.insertInputElValue();
-  }
-
-  result(){
-    this.inputEl.value = eval( this.inputEl.value );
-  }
-
-  insertInputElValue(){
-    this.inputEl.value  = this.inputValueArr.join('');
+    return type;
   }
 
 }
 
-new Calculator('js-keypad', 'js-input');
+new Calculator('js-keypad', 'js-process', 'js-result');
 
 
 
