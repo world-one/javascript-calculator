85c0b9fb5723bbedffc524ff3b353d5667d131d4
world-one <worldone85@gmail.com>
2020-06-17 21:14:29 +0900

    [20190617]server.js

--- server.js
+++ server.js
@@ -7,11 +7,46 @@ const onRequest = (request, response) => {
   let filePath = '.' + request.url;
   if (filePath == './')
       filePath = './index.html';
-
+  
   let contentType = 'text/html';
-  fs.readFile(filePath, function(error, content) {    
-    response.writeHead(200, { 'Content-Type': contentType });
-    response.end(content, 'utf-8');
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.gif': 'image/gif',
+      '.wav': 'audio/wav',
+      '.mp4': 'video/mp4',
+      '.woff': 'application/font-woff',
+      '.ttf': 'application/font-ttf',
+      '.eot': 'application/vnd.ms-fontobject',
+      '.otf': 'application/font-otf',
+      '.svg': 'application/image/svg+xml'
+  };
+
+  contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+        if(error.code == 'ENOENT'){
+          fs.readFile('./404.html', function(error, content) {
+              response.writeHead(200, { 'Content-Type': contentType });
+              response.end(content, 'utf-8');
+          });
+        }
+        else {
+          response.writeHead(500);
+          response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+          response.end();
+        }
+    }
+    else {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+    }
   });
 }
 http.createServer(onRequest).listen(8888);
